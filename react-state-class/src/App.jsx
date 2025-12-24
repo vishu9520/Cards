dcc92884260b91c18d7722c92e4e0a8d35b98403
vishu vatsay <vishusharma9520@gmail.com>
2025-12-24 20:54:13 +0530

    Ludo

--- react-state-class/src/App.jsx
+++ react-state-class/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Counter from './Counter.jsx'
 import LikeButton from './LikeButton.jsx'
+import Ludo from './LudoBoard.jsx'
 function App() {
  return( <>
   <h1>states in react
     
-    <LikeButton/> 
-    <Counter/>
+    {/* <LikeButton/> 
+    <Counter/> */}
+    <Ludo/>
   </h1>
  
     </>
---
+++ react-state-class/src/LudoBoard.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+export default function LudoBoard(){
+    let [moves,setMoves]=useState({blue: 0,yellow:0,green:0,red:0 });
+    let updateBule =()=>{
+       
+        console.log(moves.blue);
+        setMoves((prevMoves)=>{return{...prevMoves ,blue: prevMoves.blue +1}}  );
+    }
+    let updateYellow =()=>{
+       
+        console.log(moves.yellow);
+        setMoves((prevMoves)=>{return{...prevMoves ,yellow: prevMoves.yellow +1}});
+    }
+    let updateGreen =()=>{
+       
+        console.log(moves.green);
+        setMoves((prevMoves)=>{return{...prevMoves ,green: prevMoves.green +1}});
+    }
+    let updateRed =()=>{
+       
+        console.log(moves.red);
+        setMoves((prevMoves)=>{return{...prevMoves ,red: prevMoves.red +1}});
+    }
+     
+    
+    return(
+        <div>
+            <p>Game Begins!</p>
+            <div className="board">
+                <p>Blue Moves ={moves.blue}</p>
+                <button style={{backgroundColor :"blue"}} onClick={updateBule}>+1</button>
+                <p>Yellow Moves ={moves.yellow}</p>
+                <button style={{backgroundColor :"yellow"}} onClick={updateYellow}>+1</button>
+                <p>Green Moves ={moves.green}</p>
+                <button style={{backgroundColor :"green"}} onClick={updateGreen}>+1</button>
+                <p>Red Moves ={moves.red}</p>
+                <button style={{backgroundColor :"red"}} onClick={updateRed}>+1</button>
+            </div>
+        </div>
+    );
+}
